47caa00c018d1d6f10592871776b7377a6bcee43
Philippa Dufana <68788485+Philippa29@users.noreply.github.com>
2024-04-23 12:06:30 +0200

    Agent and Applicant end points created

---
+++ eKhaya/9.1.0/aspnet-core/src/eKhaya.Application/Services/AgentService/AgentMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using eKhaya.Authorization.Users;
+using eKhaya.Domain.Users;
+using eKhaya.Services.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eKhaya.Services.AgentService
+{
+    public class AgentMappingProfile : Profile
+    {
+        public AgentMappingProfile()
+        {
+            CreateMap<Agent, AgentDto>();
+
+            //.ForMember(e => e.RoleNames, d => d.Ignore());
+            CreateMap<CreateAgentDto, Agent>();
+            CreateMap<CreateAgentDto, User>()
+                .ForMember(e => e.Id, d => d.Ignore());
+            CreateMap<AgentDto, User>()
+                .ForMember(e => e.Id, d => d.Ignore());
+            //.ForMember(x => x.Roles, m  => m.MapFrom(x => x.RoleNames )); //ignores keys 
+
+
+        }
+    }
+}
--- eKhaya/9.1.0/aspnet-core/src/eKhaya.Application/Services/ApplicantService/IApplicantAppService.cs
+++ eKhaya/9.1.0/aspnet-core/src/eKhaya.Application/Services/ApplicantService/IApplicantAppService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Abp.Application.Services;
+using eKhaya.Services.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace eKhaya.Services.ApplicantService
 {
-    internal class IApplicantAppService
+    public interface IApplicantAppService : IApplicationService
+
     {
+        Task<ApplicantDto> CreateApplicantAsync(CreateApplicantDto input);
+
+        Task<ApplicantDto> UpdateApplicantAsync(ApplicantDto input);
+
+        Task<ApplicantDto> GetApplicantAsync(Guid id);
+
+        Task<List<ApplicantDto>> GetAllApplicantsAsync();
+
+        Task DeleteApplicantAsync(Guid id);
     }
 }
