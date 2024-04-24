6e7e83cacfb468c9d649dccf628cac1590a61c79
Philippa Dufana <68788485+Philippa29@users.noreply.github.com>
2024-04-24 20:52:47 +0200

    Address end point

---
+++ eKhaya/9.1.0/aspnet-core/src/eKhaya.Application/Services/AddressesService/IAddressAppService.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Application.Services;
+using eKhaya.Services.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eKhaya.Services.AddressesService
+{
+    public interface IAddressAppService : IApplicationService
+    {
+        Task<AddressesDto> CreateAddressAsync(AddressesDto input);
+
+        Task<AddressesDto> UpdateAddressAsync(AddressesDto input);
+
+        Task<AddressesDto> GetAddressAsync(Guid id);
+
+        Task<List<AddressesDto>> GetAllAddressesAsync();
+
+        Task DeleteAddressAsync(Guid id);
+    }
+}
