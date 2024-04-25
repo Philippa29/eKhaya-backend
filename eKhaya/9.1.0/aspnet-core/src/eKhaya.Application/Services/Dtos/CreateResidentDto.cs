03cbe8692e47f7198f4c8a60dc1788c16cf8fde7
Philippa Dufana <68788485+Philippa29@users.noreply.github.com>
2024-04-25 12:48:26 +0200

    Feature resident - updated resident and applicant  (#6)
    * removed unnecessary folders
    
    * Agent Endpoint
    
    updated the agent endpoint , tested it on swagger .
    
    * PropertyManager endpoint
    
    * Applicant endpoint
    
    * Update ApplicantDto.cs
    
    * Resident and APplicant endpoints updated and work on swagger

--- eKhaya/9.1.0/aspnet-core/src/eKhaya.Application/Services/Dtos/CreateResidentDto.cs
+++ eKhaya/9.1.0/aspnet-core/src/eKhaya.Application/Services/Dtos/CreateResidentDto.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace eKhaya.Services.Dtos
 {
-    public class CreateApplicantDto : PersonDto
+    public class CreateResidentDto : PersonDto
     {
-        [StringLength(13)]
-        public string ApplicantId { get; set; }
         public string Name { get; set; }
 
         public string Surname { get; set; }
