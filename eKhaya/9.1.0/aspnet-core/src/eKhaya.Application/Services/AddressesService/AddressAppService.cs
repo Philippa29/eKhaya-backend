440d0b838e9cea3cce6388ae0ffbfddbec9e8f37
Philippa Dufana <68788485+Philippa29@users.noreply.github.com>
2024-04-25 12:48:41 +0200

    Feature address (#7)
    * removed unnecessary folders
    
    * Agent Endpoint
    
    updated the agent endpoint , tested it on swagger .
    
    * PropertyManager endpoint
    
    * Applicant endpoint
    
    * Update ApplicantDto.cs
    
    * Resident and APplicant endpoints updated and work on swagger
    
    * Address end point

---
+++ eKhaya/9.1.0/aspnet-core/src/eKhaya.Application/Services/AddressesService/AddressAppService.cs
@@ -0,0 +1,63 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using eKhaya.Domain.Address;
+using eKhaya.Services.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eKhaya.Services.AddressesService
+{
+    public class AddressAppService : ApplicationService , IAddressAppService
+    {
+        private readonly IRepository<Addresses, Guid> _addressRepository;
+
+        public AddressAppService(IRepository<Addresses, Guid> addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+
+
+        public async Task<AddressesDto> CreateAddressAsync(AddressesDto input)
+        {
+            var address = ObjectMapper.Map<Addresses>(input);
+            var newAddress = await _addressRepository.InsertAsync(address);
+            
+            return ObjectMapper.Map<AddressesDto>(newAddress);
+        }
+
+        public async Task DeleteAddressAsync(Guid id)
+        {
+            await _addressRepository.DeleteAsync(id);
+        }
+
+        public async Task<AddressesDto> GetAddressAsync(Guid id)
+        {
+            var address = await _addressRepository.GetAsync(id);
+            return ObjectMapper.Map<AddressesDto>(address);
+
+        }
+
+        public async Task<List<AddressesDto>> GetAllAddressesAsync()
+        {
+            var addresses = await _addressRepository.GetAllListAsync();
+            return ObjectMapper.Map<List<AddressesDto>>(addresses);
+        }
+
+        public async Task<AddressesDto> UpdateAddressAsync(AddressesDto input)
+        {
+            var address = await _addressRepository.GetAsync(input.Id);
+            ObjectMapper.Map(input, address);
+            var updatedAddress = await _addressRepository.UpdateAsync(address);
+            
+            return ObjectMapper.Map<AddressesDto>(updatedAddress);
+        }
+
+     
+    }
+
+
+}
+
---
+++ eKhaya/9.1.0/aspnet-core/src/eKhaya.Application/Services/Dtos/AddressesDto.cs
@@ -0,0 +1,38 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eKhaya.Services.Dtos
+{
+    public class AddressesDto : EntityDto<Guid>
+    {
+        
+
+        public  string AddressLine1 { get; set; }
+
+        
+        public  string AddressLine2 { get; set; }
+
+        
+        public string AddressLine3 { get; set; }
+
+        
+        public  string Suburb { get; set; }
+
+        
+        public string Town { get; set; }
+
+        
+        public string POBox { get; set; }
+
+        
+        public decimal? Latitude { get; set; }
+
+        
+        public decimal? Longitude { get; set; }
+    }
+}
