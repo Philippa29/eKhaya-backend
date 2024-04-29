0cfad06acd5866f65a9d28dfe1493fea6cb7f716
Philippa Dufana <68788485+Philippa29@users.noreply.github.com>
2024-04-29 19:33:55 +0200

    Units Amenities endpoint

---
+++ eKhaya/9.1.0/aspnet-core/src/eKhaya.Application/Services/Dtos/UnitsAmenitiesDto.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eKhaya.Services.Dtos
+{
+    public class UnitsAmenitiesDto : EntityDto<Guid>
+    {
+        public Guid UnitId { get; set; }
+        public Guid AmenityId { get; set; }
+    }
+}
---
+++ eKhaya/9.1.0/aspnet-core/src/eKhaya.Application/Services/UnitsAmenitiesAppService/UnitsAmenitiesMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using eKhaya.Domain.UnitsAmenities;
+using eKhaya.Services.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eKhaya.Services.UnitsAmenitiesAppService
+{
+    public class UnitsAmenitiesMappingProfile :Profile 
+    {
+        public UnitsAmenitiesMappingProfile()
+        {
+            CreateMap<UnitsAmenities, UnitsAmenitiesDto>()
+                .ForMember(dest => dest.UnitId, opt => opt.MapFrom(src => src.Unit != null ? src.Unit.Id : (Guid?)null))
+                .ForMember(dest => dest.AmenityId, opt => opt.MapFrom(src => src.Amenity != null ? src.Amenity.Id : (Guid?)null));
+            CreateMap<Dtos.UnitsAmenitiesDto, UnitsAmenities>()
+                .ForMember(dest => dest.Unit, opt => opt.Ignore())
+                .ForMember(dest => dest.Amenity, opt => opt.Ignore());
+            
+        }
+    }
+}
